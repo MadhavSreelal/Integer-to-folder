6c6bb2b4c7ebef0e03ba09826346589e6570e27b
MadhavSreelal <madhavsreelal4@gmail.com>
2024-11-19 19:47:57 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+class RomanConverter:
+    
+    def int_to_roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syms[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+
+num = int(input("Enter an integer number: "))
+
+rc = RomanConverter()
+
+roman_value = rc.int_to_roman(num)
+
+print(f"The equivalent roman value for {num} is: {roman_value}")
